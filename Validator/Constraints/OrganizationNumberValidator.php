f113db228a2cfa2a7747865db1710a21827906d9
Jon Gotlin <jon@jon.se>
2018-12-17 10:35:08 +0100

    Allow identity and coordination number as organization number

--- Validator/Constraints/OrganizationNumberValidator.php
+++ Validator/Constraints/OrganizationNumberValidator.php
@@ -2,8 +2,10 @@
 
 namespace JGI\IdentityNumberValidatorBundle\Validator\Constraints;
 
+use byrokrat\id\CoordinationIdFactory;
 use byrokrat\id\OrganizationIdFactory;
 use byrokrat\id\Exception;
+use byrokrat\id\PersonalIdFactory;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -12,14 +14,30 @@ class OrganizationNumberValidator extends ConstraintValidator
     /**
      * @var OrganizationIdFactory
      */
-    protected $factory;
+    protected $organizationIdFactory;
+
+    /**
+     * @var PersonalIdFactory|null
+     */
+    protected $personalIdFactory;
+
+    /**
+     * @var CoordinationIdFactory|null
+     */
+    protected $coordinationIdFactory;
 
     /**
      * @param OrganizationIdFactory $factory
      */
-    public function __construct(OrganizationIdFactory $factory)
+    public function __construct(
+        OrganizationIdFactory $organizationIdFactory,
+        PersonalIdFactory $personalIdFactory = null,
+        CoordinationIdFactory $coordinationIdFactory = null
+    )
     {
-        $this->factory = $factory;
+        $this->organizationIdFactory = $organizationIdFactory;
+        $this->personalIdFactory = $personalIdFactory;
+        $this->coordinationIdFactory = $coordinationIdFactory;
     }
 
     /**
@@ -41,8 +59,30 @@ class OrganizationNumberValidator extends ConstraintValidator
             }
         }
 
+        if ($constraint->allowPersonalIdNumber) {
+            if (!$this->personalIdFactory) {
+                throw new \LogicException('$personalIdFactory is not configured');
+            }
+            try {
+                $this->personalIdFactory->createId($value);
+
+                return;
+            } catch (Exception $e) {}
+        }
+
+        if ($constraint->allowCoordinationNumber) {
+            if (!$this->coordinationIdFactory) {
+                throw new \LogicException('$coordinationIdFactory is not configured');
+            }
+            try {
+                $this->coordinationIdFactory->createId($value);
+
+                return;
+            } catch (Exception $e) {}
+        }
+
         try {
-            $this->factory->createId($value);
+            $this->organizationIdFactory->createId($value);
         } catch (Exception $e) {
             $this->context->addViolation($constraint->message);
         }
